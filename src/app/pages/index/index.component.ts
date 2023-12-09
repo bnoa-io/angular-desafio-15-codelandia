340d03dc0c519fc7338aa4e69978d6603e719444
bnoa-io <brayanapeles.dev@gmail.com>
2023-12-09 03:22:08 -0300

    section differential created

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WeAreComponent } from './sections/we-are/we-are.component';
 import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
 import { ContactComponent } from './sections/contact/contact.component';
+import { DifferentialComponent } from './sections/differential/differential.component';
 
 @Component({
   selector: 'app-sections',
@@ -9,11 +10,14 @@ import { ContactComponent } from './sections/contact/contact.component';
   imports: [
     WeAreComponent,
     TestimonialsComponent,
-    ContactComponent
+    ContactComponent,
+    DifferentialComponent
   ],
   template: `<app-we-are></app-we-are>
+    <app-differential></app-differential>
     <app-testimonials></app-testimonials>
-    <app-contact></app-contact>`,
+    <app-contact></app-contact>
+    `,
   styles: ''
 })
 export class PageIndexComponent {
---
+++ src/app/pages/index/sections/differential/differential.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-differential',
+  standalone: true,
+  imports: [],
+  templateUrl: './differential.component.html',
+  styleUrl: './differential.component.css'
+})
+export class DifferentialComponent {
+
+}
