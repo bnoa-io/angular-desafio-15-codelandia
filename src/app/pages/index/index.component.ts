2872dffdef51c53a69ec2767636110e9f25fd6df
bnoa-io <brayanapeles.dev@gmail.com>
2023-12-09 01:48:51 -0300

    feat section contact

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { WeAreComponent } from './sections/we-are/we-are.component';
 import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @Component({
   selector: 'app-sections',
   standalone: true,
   imports: [
     WeAreComponent,
-    TestimonialsComponent
+    TestimonialsComponent,
+    ContactComponent
   ],
   template: `<app-we-are></app-we-are>
-    <app-testimonials></app-testimonials>`,
+    <app-testimonials></app-testimonials>
+    <app-contact></app-contact>`,
   styles: ''
 })
 export class PageIndexComponent {
