c92b831329d9d889cc7877eaf430b778f438c3a7
bnoa-io <brayanapeles.dev@gmail.com>
2023-12-10 00:00:45 -0300

    feat section venues created

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -3,6 +3,7 @@ import { WeAreComponent } from './sections/we-are/we-are.component';
 import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
 import { ContactComponent } from './sections/contact/contact.component';
 import { DifferentialComponent } from './sections/differential/differential.component';
+import { VenuesComponent } from './sections/venues/venues.component';
 
 @Component({
   selector: 'app-sections',
@@ -11,10 +12,13 @@ import { DifferentialComponent } from './sections/differential/differential.comp
     WeAreComponent,
     TestimonialsComponent,
     ContactComponent,
-    DifferentialComponent
+    DifferentialComponent,
+    VenuesComponent
   ],
-  template: `<app-we-are></app-we-are>
+  template: `
+    <app-we-are></app-we-are>
     <app-differential></app-differential>
+    <app-venues></app-venues>
     <app-testimonials></app-testimonials>
     <app-contact></app-contact>
     `,
---
+++ src/app/pages/index/sections/venues/venues.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venues',
+  standalone: true,
+  imports: [],
+  templateUrl: './venues.component.html',
+  styleUrl: './venues.component.css'
+})
+export class VenuesComponent {
+
+}
